d5b15fe07206e0c681cd7d75d1de740582672e74
Donghyun Kim <digitalisx99@gmail.com>
2018-01-01 10:23:17 +0900

    setup.py Error Fix

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=install_requires,
     setup_requires=setup_requires,
     scripts=[
-        'scripts/filehash',
-        'scripts/jpgscan'
+        'scripts/filehash.py',
+        'scripts/jpgscan.py'
     ],
 )
